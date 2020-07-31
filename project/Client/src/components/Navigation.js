1b7f0f1d18d93280c578beb1db1dae99842dea22
Help <pczerokey@gmail.com>
2020-07-30 22:41:32 -0400

    fix wrong import

--- project/Client/src/components/Navigation.js
+++ project/Client/src/components/Navigation.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Auth0Provider } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Navigation = (props) => {
-    const {isAuthenticated, loginWithRedirect, logout} = Auth0Provider();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   return (
     <div className="header">
-        {!isAuthenticated && (
-            <ul className="nav nav-pills pull-right">
-                <li><Link to="/">Home</Link></li>
-                <button onClick={() => loginWithRedirect({})}>Log in</button>
-            </ul>
-        )}
-        {isAuthenticated && (
-            <ul className="nav nav-pills pull-right">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <button onClick={() => logout()}>Log out</button>
-            </ul>
-        )}
+      {!isAuthenticated && (
+        <ul className="nav nav-pills pull-right">
+          <li><Link to="/">Home</Link></li>
+          <button onClick={() => loginWithRedirect({})}>Log in</button>
+        </ul>
+      )}
+      {isAuthenticated && (
+        <ul className="nav nav-pills pull-right">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <button onClick={() => logout()}>Log out</button>
+        </ul>
+      )}
 
       <h3 className="text-muted">Securing React</h3>
     </div>
   );
-}
+};
 
-export default Navigation; 
+export default Navigation;
