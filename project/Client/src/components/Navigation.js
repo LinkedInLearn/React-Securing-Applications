b28fbee0b890b5320e01c098a54dd7c58ecf3e26
Help <pczerokey@gmail.com>
2020-07-30 22:37:41 -0400

    Add authentication pages and views depending on logged in

--- project/Client/src/components/Navigation.js
+++ project/Client/src/components/Navigation.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const Navigation = (props) => {
+    const {isAuthenticated, loginWithRedirect, logout} = Auth0Provider();
   return (
     <div className="header">
-      <ul className="nav nav-pills pull-right">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-      </ul>
+        {!isAuthenticated && (
+            <ul className="nav nav-pills pull-right">
+                <li><Link to="/">Home</Link></li>
+                <button onClick={() => loginWithRedirect({})}>Log in</button>
+            </ul>
+        )}
+        {isAuthenticated && (
+            <ul className="nav nav-pills pull-right">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <button onClick={() => logout()}>Log out</button>
+            </ul>
+        )}
+
       <h3 className="text-muted">Securing React</h3>
     </div>
   );
