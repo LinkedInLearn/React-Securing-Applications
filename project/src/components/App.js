8a7b71a9ecbb4f666d57c76ed06a28715ae50210
Help <pczerokey@gmail.com>
2020-07-30 21:20:28 -0400

    fix package.json eslint package problem, and example of dangerous code

--- project/src/components/App.js
+++ project/src/components/App.js
@@ -8,40 +8,59 @@ import About from './About';
 import data from '../data/data.json';
 import './App.css';
 
+const createMarkup = () => {
+  return {_html: 'I\'m so dangerous you could feel it'}
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state ={
-      name: "Manny Henri",
-      jumbotronTitle: "List of courses",
+    this.state = {
+      name: 'Manny Henri',
+      jumbotronTitle: 'List of courses',
       feeds: [],
-    }
+    };
   }
 
+  // eslint-disable-next-line camelcase
   UNSAFE_componentWillMount() {
     this.setState({
       feeds: data,
-    })
+    });
   }
+
   render() {
     return (
+    // eslint-disable-next-line react/jsx-filename-extension
       <Router>
         <div className="container">
           <Navigation />
-          <Jumbotron title={this.state.jumbotronTitle}/>
+          {/* eslint-disable-next-line react/destructuring-assignment */}
+          <Jumbotron title={this.state.jumbotronTitle} />
           <Switch>
-            <Route path="/contact" component={Contact}/>
-            <Route path="/about" component={About}/>
-            <Route exact path="/" render={(props) => (
-              <Feed feeds={this.state.feeds} />
-            )} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+            <Route
+              exact
+              path="/"
+              /* eslint-disable-next-line no-unused-vars */
+              render={(props) => (
+                <Feed feeds={this.state.feeds} />
+              )}
+            />
           </Switch>
           <div className="footer">
-                <p>&copy; {this.state.name} Inc.</p>
+            <p>
+              &copy;
+              {this.state.name}
+              {' '}
+              Inc.
+            </p>
+            <div innerHTML={createMarkup()}/>
           </div>
         </div>
       </Router>
-    )
+    );
   }
 }
 
